ff5860e9cdf4e9a79fa0a1343cde3bd160ed5e14
Calem Bendell <calem.j.bendell@gmail.com>
2015-01-11 18:13:39 -0500

    initialising

---
+++ Assets/gravity_sphere.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gravity_sphere : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay (Collider other) {
+		float gravitational_constant = 0.005f;
+		float distance = this.transform.localScale.x - Vector3.Distance (other.gameObject.transform.position, this.transform.position);
+		other.gameObject.transform.position = Vector3.MoveTowards(other.gameObject.transform.position, this.transform.position, gravitational_constant * other.rigidbody.mass * distance);
+	}
+}
